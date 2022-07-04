942276cd74a6224a79dcfa096a0e86b3648c1237
DELL\necoj <nemanja.jovic195@gmail.com>
2022-07-04 18:23:18 +0200

    Added PostsList component and some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import PostsList from "./features/posts/PostsList";
+
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <PostsList />
+    </main>
+  );
 }
 
 export default App;
