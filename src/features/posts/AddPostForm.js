5f2ca6e3e753789b4408ec894d22cce6fbfa9c59
DELL\necoj <nemanja.jovic195@gmail.com>
2022-07-04 18:44:09 +0200

    Added form for input

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+
+  return (
+    <section>
+      <h2>Add a new post</h2>
+      <form>
+        <label htmlFor="postTitle">Post title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <input
+          type="text"
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+      </form>
+    </section>
+  );
+};
+
+export default AddPostForm;
